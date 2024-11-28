c4ada4c9ae3da003d8eb8887f0f61ae50baf428f
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2024-11-28 12:45:27 +0530

    done

--- todo_authentication/settings.py
+++ todo_authentication/settings.py
@@ -126,8 +126,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Default static directory
+    os.path.join(BASE_DIR, "todo_authentication/static"),
+     # '/home/harikrushn/Downloads/project/todo_authentication/static',
+    # '/home/harikrushn/Downloads/project/todo_authentication/todo_authentication/static',
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/"),
+]
