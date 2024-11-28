80847ea104c5ad67bf8ef680f2b99fa492057fc7
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2024-11-28 12:20:44 +0530

    done

--- todos/models.py
+++ todos/models.py
@@ -3,8 +3,17 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(max_length=14)
     password = models.CharField(max_length=255)
-    gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')], default='Male')
+    gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')], default='Select')
     age = models.PositiveIntegerField()
     profession = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.phone.startswith('+91-'):
+            self.phone = f'+91-{self.phone}'
+        super().save(*args, **kwargs)
+        
